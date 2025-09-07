008b764489c9f5dd7e223b74ea3e8b0eb319716e
KaitoMuraoka <kaitoboshi@icloud.com>
2025-09-07 12:31:26 +0900

    rails g model User name:string を実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_06_134235) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_07_033014) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
