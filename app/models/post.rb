a84f83979dcb849bab553dbcf7dffd3ee430e4d8
KaitoMuraoka <kaitoboshi@icloud.com>
2025-09-07 12:09:57 +0900

    バリデーションを追加して入力データを制限する

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  # 一意性, ユニークである(タイトルが他のと被らない)
+  validates :title, presence: true, length: { maximum: 10 }, uniqueness: true # 空文字は禁止、最大文字数10文字、ユニークである
+  validates :body, presence: true, length: { minimum: 5 }
 end
